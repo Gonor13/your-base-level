39f2fc37c62190d64d2f1981741a07307a18a5c1
Gonor13 <petrenkonikita13@yandex.ru>
2026-01-29 22:25:31 +0300

    Add complete Base Level analytics with real-time simulation

--- public/ethers.js
+++ public/ethers.js
@@ -1,3 +1,24 @@
-﻿// Minimal ethers.js v5.7.2 (compressed)
-!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.ethers=t():e.ethers=t()}("undefined"!=typeof self?self:this,function(){return function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}return n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){"use strict";e.exports=n(1)},function(e,t,n){"use strict";function r(e,t){for(var n=0;n<t.length;n++){var r=t[n];r.enumerable=r.enumerable||!1,r.configurable=!0,"value"in r&&(r.writable=!0),Object.defineProperty(e,r.key,r)}}function o(e,t,n){return t&&r(e.prototype,t),n&&r(e,n),e}n.d(t,"a",function(){return o})}])});
-console.log('Minimal ethers.js loaded');
+﻿// Minimal ethers.js fallback - basic wallet connection only
+window.ethers = {
+    providers: {
+        Web3Provider: class Web3Provider {
+            constructor(provider) {
+                this.provider = provider;
+            }
+            getSigner() {
+                return {
+                    getAddress: async () => {
+                        const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+                        return accounts[0];
+                    }
+                };
+            }
+        }
+    },
+    utils: {
+        formatEther: (wei) => {
+            return (parseInt(wei) / 1e18).toFixed(4);
+        }
+    }
+};
+console.log('Using minimal ethers.js fallback');
